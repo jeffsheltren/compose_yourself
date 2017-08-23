850f9fce4e760d7f3138496c87df6b6f71455798
Narayan Newton <nnewton@tag1consulting.com>
2017-08-23 15:22:58 -0700

    convert fully to arguments and remove debug

--- compose_yourself.sh
+++ compose_yourself.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-set -x
+
 ####
 #Script takes two arguments, the source tag and the deploy tag
 ####
 
-SRC_REPO="git@github.com:tag1consulting/quo.tag1consulting.com.git"
-TARGET_REPO="git@github.com:tag1consulting/quo-deploy.git"
 TMP_DIR="/tmp"
 NOW=$(date +"%m_%d_%Y")
 
 ###Arguments
 
-if [ $# -ne 2 ]; then
-  echo 'Please provide a source tag and a deploy tag, like so:'
-  echo 'compose_yourself.sh \<source_tag\> \<deploy_tag\>'
+if [ $# -ne 4 ]; then
+  echo 'Please provide a source repo and tag and a deploy repo and tag, like so:'
+  echo 'compose_yourself.sh <source_repo> <source_tag> <deploy_repo> <deploy_tag>'
   exit 1
 fi
 
-SRC_TAG=$1
-TARGET_TAG=$2
+SRC_REPO=$1
+SRC_TAG=$2
+
+TARGET_REPO=$3
+TARGET_TAG=$4
 
 ###Cleanup
 
@@ -68,7 +69,12 @@ popd
 ###Commit
 
 git remote add deploy ${TARGET_REPO} || exit 1
-git add web
-git add vendor
+git add web || exit 1
+git add vendor || exit 1
 git commit -m "Composed Deploy Tree For Tag ${SRC_TAG}" || exit 1
 git push deploy ${SRC_TAG}:${TARGET_TAG}_$NOW || exit 1
+
+###Cleanup
+popd
+popd
+rm -rf "${TMP_DIR}/compose_yourself_work"
